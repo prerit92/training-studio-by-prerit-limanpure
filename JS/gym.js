cf97e014d195dd282bd4d173e66cbc7b5cb53a6e
prerit92 <92568512+prerit92@users.noreply.github.com>
2021-11-09 15:12:33 +0530

    Update gym.js

--- JS/gym.js
+++ JS/gym.js
@@ -1,3 +1,10 @@
+// ===========Preloader===========
+let preloader = document.getElementById('preloader');
+
+window.addEventListener('load', function () {
+    preloader.style.display = "none";
+})
+
 let humburger = document.querySelector('.humburger');
 
 let navbar = document.querySelector('.navbar');
